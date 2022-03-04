9cdd5aabb05f1b2bde59d579c626670ae7456b1b
Cheng Wang <wachmc@gmail.com>
2022-03-04 19:02:00 +0100

    Cache built contracts

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,11 @@
-import * as alephium from "alephium-js"
-import { promises as fsPromises } from "fs"
-import * as crypto from "crypto-js"
 import { CliqueClient } from "alephium-js"
-
-class Artifact {
-    contractSha256: string
-    bytecode: string
-
-    constructor(contractSha256: string, bytecode: string) {
-        this.contractSha256 = contractSha256
-        this.bytecode = bytecode
-    }
-
-    static async fromContractPath(client: CliqueClient, fileName: string): Promise<Artifact> {
-        const contract = await fsPromises.readFile(`./contracts/${fileName}`)
-        const contractStr = contract.toString()
-        const contractHash = crypto.SHA256(contractStr).toString()
-        const compiled = await client.contracts.postContractsCompileContract({code: contractStr})
-
-        return new Artifact(contractHash, compiled.data.code)
-    }
-
-    toString(): string {
-        return JSON.stringify({ contractSha256: this.contractSha256, bytecode: this.bytecode }, null, 2)
-    }
-}
+import { Contract } from "./contract"
 
 async function test() {
-    const client = new alephium.CliqueClient({baseUrl: "http://127.0.0.1:12973"})
+    const client = new CliqueClient({baseUrl: "http://127.0.0.1:12973"})
     await client.init(false)
 
-    const x = await Artifact.fromContractPath(client, "add.ral")
+    const x = await Contract.fromContractPath(client, "add.ral")
     console.log(`hello ${x}`)
 }
 
